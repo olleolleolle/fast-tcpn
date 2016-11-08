536e3c97b1e2bf0ba36b75815460f96139e76448
Wojciech Rzasa <me@wojciechrzasa.pl>
2016-11-08 11:59:44 +0100

    Don't clone frozen objects (causes error anyway...)

--- lib/fast-tcpn/clone/using_code_from_stack.rb
+++ lib/fast-tcpn/clone/using_code_from_stack.rb
@@ -12,6 +12,7 @@ end
 class Object
   def deep_clone
     return @deep_cloning_obj if @deep_cloning
+    return @deep_cloning_obj if frozen?
     @deep_cloning_obj = clone
     @deep_cloning_obj.instance_variables.each do |var|
       val = @deep_cloning_obj.instance_variable_get(var)
